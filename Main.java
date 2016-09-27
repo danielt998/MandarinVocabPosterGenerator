3d795d81490c3e5a971cc3b10a8c6b6e5da49536
Daniel Thomas <mbax3dt2@student.manchester.ac.uk>
2016-09-27 12:33:04 +0100

    small tidy-ups

--- Main.java
+++ Main.java
@@ -10,14 +10,13 @@ public class Main extends JPanel{
 	public static int X_CONST=100;
 	public static int Y_CONST=50;
 	public static int count=0;
-	//public void paint(Graphics g){
-//
 	public static void paintStuff(){
-			BufferedImage bi=new BufferedImage(3000,2000,
-																				BufferedImage.TYPE_INT_RGB);
-
-		//Graphics2D g2=(Graphics2D)g;
+		BufferedImage bi=new BufferedImage(3000,2000,
+																				BufferedImage.TYPE_INT_BGR);
 		Graphics2D g2 = bi.createGraphics();
+		g2.setColor(Color.WHITE);
+		g2.fillRect(0,0,g2.getWidth(),g2.getHeight());
+		g2.setColor(Color.BLACK);
 		Font font = new Font("Serif", Font.PLAIN, 40);
     g2.setFont(font);
 		try (BufferedReader br = new BufferedReader(
@@ -28,20 +27,12 @@ public class Main extends JPanel{
      	  count++;
      	  int x=X_CONST*(count%30);
 				int y=Y_CONST*(count/30+1);
-//	System.out.println("x:"+x+"y:"+y+"count:"+count);
 				g2.drawString(line, x,y);
    		}
-
-			//Graphics2D cg=bi.createGraphics();
-			//paintAll(cg);
-			ImageIO.write(bi, "png", new File("./output_image.png"));
+			ImageIO.write(bi, "jpg", new File("./output_image.jpg"));
 		}catch(Exception e){e.printStackTrace();} 
 	}
 	public static void main(String[] args){
-      //JFrame f = new JFrame();
-      //f.getContentPane().add(new Main());
 			paintStuff();
-      //f.setSize(1800, 1000);
-      //f.setVisible(true);
 	}
 }
