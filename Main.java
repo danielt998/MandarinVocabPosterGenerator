5c1692c45bf3d6a64b5cf30dab38cd19a6f4b811
daniel@fearnley.net <cawscaws28>
2020-05-09 17:56:30 +0100

    Make a start on calculating ratios and stuff in a reasonable way

--- Main.java
+++ Main.java
@@ -11,13 +11,20 @@ public class Main extends JPanel{
 
   public static int X_CONST=220;
   public static int Y_CONST=100;
+  public static int charRatio = X_CONST / Y_CONST;
   public static int count=0;
 
   public static String FILE_PATH = "HSK6";/*"pleco-out.txt"*/
 
+  private static final double pageRatio = 1.618; //TODO: maybe allow other formats e.g. letter, portrait, ...
+
+  private static final int xWidth = 6000;
+
+  //TODO:THERE ARE A LOT OF POTENTIAL precision issues and stuff!!
   public static void paintStuff() throws IOException {
-    BufferedImage bi=new BufferedImage(6000,4000,
-                                        BufferedImage.TYPE_INT_BGR);
+//    BufferedImage bi=new BufferedImage(6000,4000,
+  //                                      BufferedImage.TYPE_INT_BGR);
+    BufferedImage bi = new BufferedImage(xWidth, (int)(xWidth/ pageRatio), BufferedImage.TYPE_INT_BGR);
     Extract.readInDictionary();
     Graphics2D g2 = bi.createGraphics();
 //    g2.setColor(Color.WHITE);
@@ -27,21 +34,29 @@ public class Main extends JPanel{
     Font pinyinFont = new Font("Serif", Font.PLAIN, 15);
     Font latinFont = new Font("Serif", Font.PLAIN, 10);
 
-    final long noLines = Files.lines(Paths.get(FILE_PATH)).count();
+    final long noLines = Files.lines(Paths.get(FILE_PATH)).count();//+1? as we seem to start at 0 or smth (first square is blank - though that needs fixing)
 
     try (BufferedReader br = new BufferedReader(
                                 new FileReader(FILE_PATH))) {
       String line;
       while ((line = br.readLine()) != null&&count<noLines) {
-        int NO_COLS = 26;
+        //int noCols = //26;
+      //  int noRows = (int)Math.ceil(Math.sqrt(Math.ceil(noLines / pageRatio)));//TODO:is using ceil twice here too generous?
+      //  int noCols = (int) Math.ceil(noRows * pageRatio);//THIS ASSUMES width and height of items are the same (THEY ARE NOT)
+
+//        int noRows = (int)Math.ceil(Math.sqrt(noLines/(pageRatio*charRatio)));//TODO:write formulas and stuffs
+  //      int noCols = (int)(noLines/noRows);
+
+        int noRows = (int) Math.ceil(Math.sqrt((charRatio * noLines))/pageRatio);
+        int noCols = (int) (noLines/noRows);
         // process the line.
         count++;
-        int x=X_CONST*(count%NO_COLS);//30);
-        int y=Y_CONST*(count/NO_COLS+1);
+        int x=X_CONST*(count%noCols);//30);
+        int y=Y_CONST*(count/noCols+1);
         g2.setFont(font);
         g2.drawString(line, x,y);
-        int y_pinyin = (Y_CONST )*(count/NO_COLS+1) + Y_CONST/4;
-        int y_text = (Y_CONST )*(count/NO_COLS+1) + Y_CONST/2;
+        int y_pinyin = (Y_CONST )*(count/noCols+1) + Y_CONST/4;
+        int y_text = (Y_CONST )*(count/noCols+1) + Y_CONST/2;
         g2.setFont(pinyinFont);
         Word word= Extract.getWordFromChinese(line);
         String pinyin = "invalid pinyin";
